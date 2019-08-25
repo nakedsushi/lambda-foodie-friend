9a7597ca6512a0800379ffcd9daac86b9df27e6b
nakedsushi <louise.m.yang@gmail.com>
2019-08-24 22:06:52 -0700

    Can GET with hard-coded tags

--- lambda/index.js
+++ lambda/index.js
@@ -3,15 +3,32 @@ const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-  const payload = JSON.parse(event.body);
-  const operation = payload.operation;
+  let payload;
+  const operation = event.httpMethod;
+  console.log(event);
 
   switch (operation) {
-    case 'create':
+    case 'POST':
+      payload = JSON.parse(event.body);
       dynamo.put(payload, callback);
       break;
-    case 'read':
-      dynamo.get(payload, callback);
+    case 'GET':
+      payload = {
+        "TableName": "Restaurants",
+        "FilterExpression": "contains(tags, :tag1) OR contains(tags, :tag2)",
+        ExpressionAttributeValues: {
+          ":tag1": "low",
+          ":tag2": "dumplings"
+        }
+      };
+      dynamo.scan(payload, function(err, data) {
+        const response = {
+          "statusCode": 200,
+          "body": JSON.stringify(data),
+          "isBase64Encoded": false
+        };
+        callback(null, response);
+      });
       break;
     case 'update':
       dynamo.update(payload, callback);
